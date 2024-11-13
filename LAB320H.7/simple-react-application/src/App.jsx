7120752a39bce7dbffd523002d3520e28f45d215
StingySpoilero <dalaujon@gmail.com>
2024-11-13 09:44:41 -0600

    LAB320H.7

---
+++ LAB320H.7/simple-react-application/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
+
+export default function App() {
+  const apiKey = "98e3fb1f"; // Replace with your API key
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    try {
+      const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`);
+      const data = await response.json();
+      setMovie(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
